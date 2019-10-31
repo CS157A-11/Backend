7e15589292982b834ab9ecf1d5601c414267b36f
Ryoji Park <dlbia1106@gmail.com>
2019-10-31 15:03:38 -0700

    Add Cors

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,8 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
+
 require("dotenv").config();
 
 export const client = mysql.createConnection({
@@ -23,20 +25,12 @@ client.connect(err => {
 });
 
 const app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/", router);
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 export { app };
