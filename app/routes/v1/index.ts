76a2a0ba04d382a7c7536fb2c6e804a2117d0cdc
Ryoji Park <dlbia1106@gmail.com>
2019-12-02 01:11:01 -0800

    create mood of the day api

--- app/routes/v1/index.ts
+++ app/routes/v1/index.ts
@@ -4,12 +4,14 @@ import { testRouter } from "./test";
 import { todoRouter } from "./todo";
 import { userRouter } from "./user";
 import { habitRouter } from "./habit";
+import { moodOfTheDayRouter } from "./moodOfTheDay";
 
 const router = express.Router();
 
 router.use("/test", testRouter);
 router.use("/todos", todoRouter);
 router.use("/habits", habitRouter);
+router.use("/moodoftheday", moodOfTheDayRouter);
 router.use("/users", userRouter);
 
 export { router };
---
+++ app/routes/v1/moodOfTheDay.js
@@ -0,0 +1,81 @@
+import { client } from "../../app";
+import express from "express";
+import { verifyToken } from "../../utils/auth";
+
+const moodOfTheDayRouter = express.Router();
+
+moodOfTheDayRouter.use(verifyToken);
+
+moodOfTheDayRouter.post("/", (req, res, next) => {
+  console.log("POST");
+  const postData = { ...req.body, date: new Date() };
+  client.query(
+    "INSERT INTO moods_of_the_day SET ?",
+    postData,
+    (err, results) => {
+      if (err) throw err;
+      console.log(results);
+      const newUserTodo = {
+        mood_id: results.insertId,
+        email: req.email
+      };
+      client.query("INSERT INTO users_moods SET ?", newUserTodo, (err, res) => {
+        if (err) throw err;
+      });
+      res.send(postData);
+    }
+  );
+});
+
+// READ (all moods_of_the_day)
+moodOfTheDayRouter.get("/", (req, res, next) => {
+  console.log("GET");
+  console.log(req.email);
+  client.query(
+    "select * from moods_of_the_day where moods_of_the_day.mood_id in (select mood_id from users_moods where email=?)",
+    [req.email],
+    (err, results) => {
+      res.json(results);
+    }
+  );
+});
+
+// READ (single todo)
+moodOfTheDayRouter.get("/:mood_id", (req, res, next) => {
+  console.log("GET");
+  client.query(
+    "SELECT FROM moods_of_the_day where id=?",
+    [req.params.mood_id],
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
+});
+
+// UPDATE for isActive
+moodOfTheDayRouter.put("/:mood_id", (req, res, next) => {
+  console.log("PUT");
+  client.query(
+    "UPDATE moods_of_the_day SET is_active=? where id=?",
+    [req.params.isActive, req.params.mood_id],
+    function(err, results) {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
+});
+
+// DELETE
+// moodOfTheDayRouter.delete("/:mood_id", (req, res, next) => {
+//   console.log(req.body);
+//   client.query("DELETE FROM moods_of_the_day where id=?", [req.body.mood_id], function(
+//     err,
+//     results
+//   ) {
+//     if (err) throw err;
+//     res.end("Todo has been deleted!");
+//   });
+// });
+
+export { moodOfTheDayRouter };
